a764bf4d061e7a45c0f44ec90835f13fae63c160
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-05-26 13:15:12 -0600

    Initial Commit and File Complete

---
+++ greeter.js
@@ -0,0 +1,9 @@
+// // Create a variable `name` and store your name in it.
+const name = "Shawn";
+console.log(name);
+// Below this, write a line of code that displays some greeting to that name (i.e.: "Hello Barry!")
+console.log("Did you know that 'name' was depreciated in Javascript? I'm deprciating 'Hello World' as the standard. New templates be stated as...")
+console.log("");
+console.log("Hello ", name, "!");
+console.log("")
+console.log("No Egos here... :-) <---Sarcasm")
