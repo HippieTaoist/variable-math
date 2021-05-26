61ff389db549e28f92f2dd49be2b17127c43bf3c
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-05-26 12:49:56 -0600

    Complete future age coding

---
+++ future-age.js
@@ -0,0 +1,8 @@
+// Create a variable called age and set its value to your age. Now, write another line of code that logs out a sentence telling you how old you will be in five years.
+console.log("I am 41");
+const age = 41;
+console.log("In five years I....");
+const futureAge = age + 5;
+console.log("Should be 46...", "Computer Says...",
+    futureAge);
+console.log("We have a winner!");
