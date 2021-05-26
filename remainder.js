e73d4547c70dd015a9bcaa4f68def6ba817bb124
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-05-26 13:29:16 -0600

    Initial Commit and File Complete

---
+++ remainder.js
@@ -0,0 +1,6 @@
+// Create two variables that each store a positive number (name them anything you want, any positive numbers will do).
+const pos1 = 8;
+const pos2 = 2;
+const result = pos1 / pos2;
+// Log out how the remainder that is left over when the first number is divided by the second number (i.e.: "the remainder of 7 divided by 3 is 1")
+console.log("So looking at the numbers, ", pos1, " and ", pos2, " we can see the resulting division of ", pos1, " and ", pos2, " will result in the number of ", result, " and a remainder of ", pos1 % pos2), ".";
