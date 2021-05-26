f1bd439cbed6d2ee19073320a4005556cda2e4a3
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-05-26 13:06:34 -0600

    Initial commit and Project Complete

---
+++ adder.js
@@ -0,0 +1,10 @@
+// // Create two variables, `num1` and `num2`. Store any numbers in the variables. 
+console.log("Psst...I gots some nums for ya")
+const num1 = 23;
+const num2 = 45;
+console.log("Check this, it's a ", num1, " and a ", num2)
+    // Below this, add `num1` and `num2` together and store the result in a new variable (name it whatever you want).
+const num3 = num1 + num2;
+console.log("I add 'em together and viola a beautiful ", num3);
+// Finlly, log out `num1`, `num2`, and `sum` in a human readable sentence (i.e.: "8 and 5 add to 13").
+console.log("Today's prefomance was brought to you by the number ", num1, "who played 'num1', ", num2, " who dazzled us as 'num2' and last but not least number ", num3, ", showing off what you can do as 'num3' when you add the other two!")
