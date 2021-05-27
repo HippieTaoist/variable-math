f4cd6600456d54b0d7f3aee06abd5f853d476d4d
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-05-26 18:24:49 -0600

    Initial Commit and File Complete

---
+++ quadratic.js
@@ -0,0 +1,22 @@
+// Create three variables, `a`, `b`, and `c`.
+const a = 46;
+const b = 786;
+const c = 12;
+
+
+// Set them to any numbers you please as long as:
+// * a is not zero
+// * `b²` is greater than or equal to `4 × a × c`
+
+// Now use the quadratic formula (oh no) to calculate two value of two new variables, `x1` and `x2`.
+
+// <img src="https://i.ytimg.com/vi/lhQKfuAVkm4/maxresdefault.jpg" width="400px" />
+
+// Note: you'll need to do the formula twice. For `x1`, use a + in place of the ± symbol. For `x2`, use a -.
+const d = (a, c)
+
+const x1 = ((-b + Math.sqrt(Math.pow(b, 2)) - (4 * a * c))) / (2 * a);
+
+const x2 = ((-b - Math.sqrt(Math.pow(b, 2)) - (4 * a * c))) / (2 * a);
+console.log(x1);
+console.log(x2);
